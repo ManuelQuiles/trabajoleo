443a02b227e54c086aafeb1fe4776b8704c702f5
ManuelQuiles <manuuquiles@gmail.com>
2022-10-19 00:02:29 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+const Article = require('../models/article')
+const articleRouter = require('../routes/articles')
+const methodOverride = require('method-override')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+
+
+// Ruta principal Home
+app.get('/', async(req, res)=>{
+    const articles = await Article.find().sort({
+        createAt: "desc"
+    }).limit(10)
+    res.render('articles/index', {articles: articles})
+})
+
+app.get('/category/romance', async(req, res)=>{
+    let query= { category: "Romance"};
+    const articles= await Article.find(query).sort({
+        createAt: "desc"
+    }).limit(10)
+    if (articles == null) res.redirect("/");
+    res.render('articles/index', {articles: articles})
+})
+
+app.get('/category/terror', async(req, res)=>{
+    let query= { category: "Terror"};
+    const articles= await Article.find(query).sort({
+        createAt: "desc"
+    }).limit(10)
+    if (articles == null) res.redirect("/");
+    res.render('articles/index', {articles: articles})
+})
+
+app.get('/category/ficcion', async(req, res)=>{
+    let query= { category: "Ficcion"};
+    const articles= await Article.find(query).sort({
+        createAt: "desc"
+    }).limit(10)
+    if (articles == null) res.redirect("/");
+    res.render('articles/index', {articles: articles})
+})
+
+// MongoDB connection
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(()=> console.log('conectado a MongoDB Atlas'))
+    .catch((err)=> console.error(err))
+
+app.use('/articles', articleRouter);
+
+app.use('/public/', express.static('./public/'))
+
+app.listen(port,
+    ()=> console.log(`Servidor escuchando en el puerto ${port}`)
+)
